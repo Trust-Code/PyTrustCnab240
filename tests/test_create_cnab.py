99cd2c36a8f7cd908605dac24a994a07e4528ac5
Danimar Ribeiro <danimaribeiro@gmail.com>
2018-05-21 18:34:42 -0300

    [ADD] Just add a unit test to make the build pass

---
+++ tests/test_create_cnab.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# © 2018 Danimar Ribeiro <danimaribeiro@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from unittest import TestCase
+
+
+class TestCnab240(TestCase):
+
+    def test_empty(self):
+        pass
