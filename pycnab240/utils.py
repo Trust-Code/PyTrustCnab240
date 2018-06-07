5e0056495901e2251b620baeed13b31b95634b8a
Felipe <paloschi.eca@gmail.com>
2018-06-07 14:59:53 -0300

    Add TIPO_DE_SERVICO in utils

--- pycnab240/utils.py
+++ pycnab240/utils.py
@@ -20,14 +20,43 @@ FORMA_DE_LANCAMENTO = {
         16: '31',  # Liquidacao de titulos outros Bancos
     },
 }
+TIPO_DE_SERVICO = {
+    'santander': {
+        0: '03',  # Bloqueto Eletronico
+        1: '10',  # Pagamento Dividendos
+        2: '14',  # Consulta de Tributos a Pagar DETRAN com RENAVAM
+        3: '20',  # Pagamento Fornecedor
+        4: '22',  # Pagamento de Contas, Tributos e Impostos
+        5: '29',  # Alegacao do Sacado
+        6: '50',  # Pagamento Sinistros Segurados
+        7: '60',  # Pagamento Despesas Viajante em Transito
+        8: '70',  # Pagamento Autorizado
+        9: '75',  # Pagamento Credenciados
+        10: '80',  # Pagamento Representantes / Vendedores Autorizados
+        11: '90',  # Pagamento Beneficios
+        12: '98',  # Pagamentos Diversos
+    }
+}
 
 
 def get_forma_de_lancamento(bank_name, code):
     try:
         value = FORMA_DE_LANCAMENTO[bank_name][code]
     except KeyError:
-        message, value = 'Code', code
-        if not FORMA_DE_LANCAMENTO.get(bank_name):
-            message, value = 'Bank', bank_name
-        raise KeyError("{} {} not found!".format(message, value))
+        parse_keyerror(FORMA_DE_LANCAMENTO, bank_name, code)
     return value
+
+
+def get_tipo_de_servico(bank_name, code):
+    try:
+        value = TIPO_DE_SERVICO[bank_name][code]
+    except KeyError:
+        parse_keyerror(TIPO_DE_SERVICO, bank_name, code)
+    return value
+
+
+def parse_keyerror(dic, bank_name, code):
+    message, value = 'Code', code
+    if not dic.get(bank_name):
+        message, value = 'Bank', bank_name
+    raise KeyError("{} {} not found!".format(message, value))
