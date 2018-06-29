2baf0f096745b7ef3fec668ed80708394deafe5d
Danimar Ribeiro <danimaribeiro@gmail.com>
2018-06-29 00:30:14 -0300

    [WIP] Padroniza forma de lançamento, corrige campo tipo de aviso

--- pycnab240/utils.py
+++ pycnab240/utils.py
@@ -1,23 +1,13 @@
 FORMA_DE_LANCAMENTO = {
     'santander': {
-        0: '01',  # Credito em conta corrente
-        1: '03',  # Transferencias para outros bancos
-        2: '05',  # Credito em conta poupanca
-        3: '10',  # Ordem de pagamento / recibo
-        4: '11',  # Pagamento de contas com codigo de barras
-        5: '16',  # DARF Normal
-        6: '17',  # GPS - Guia de previdencia social
-        7: '18',  # DARF Simples
-        8: '20',  # Caixa "Autenticacao"
-        9: '22',  # GARE SP ICMS
-        10: '23',  # GARE SP DR
-        11: '24',  # GARE SP ITCMD
-        12: '25',  # IPVA SP
-        13: '26',  # LICENCIAMENTO SP
-        14: '27',  # DPVAT SP
-        15: '30',  # Liquidacao de titulos em carteira de cobranca proprio
-                   # Santander Banespa
-        16: '31',  # Liquidacao de titulos outros Bancos
+        '01': '03',  # Transferências para outros bancos (DOC, TED)
+        '02': '03',  # Transferências para outros bancos (DOC, TED)
+        '03': '11',  # Pagamento de Títulos
+        '04': '11',  # Tributos com código de barras
+        '05': '17',  # GPS - Guia de previdencia Social
+        '06': '16',  # DARF Normal
+        '07': '18',  # DARF Simples
+        '08': '11',  # FGTS
     },
 }
 TIPO_DE_SERVICO = {
