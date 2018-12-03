6659abd73c6795a9a5c6bf3ee3d9d3f600fca778
Danimar Ribeiro <danimaribeiro@gmail.com>
2018-12-03 18:16:51 -0200

    [FIX] O valor final de cada item de pagamento é valor_pagamento

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycnab240',
-    version='0.1.20',
+    version='0.1.21',
     author='Trustcode',
     author_email='suporte@trustcode.com.br',
     url='https://github.com/Trust-Code/PyTrustCnab240',
