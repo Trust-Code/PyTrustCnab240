489643c410596d2d11e8f84f6681975bb26b591e
Danimar Ribeiro <danimaribeiro@gmail.com>
2018-12-03 00:54:34 -0200

    [FIX] Usa os campos alfa correspondentes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycnab240',
-    version='0.1.18',
+    version='0.1.19',
     author='Trustcode',
     author_email='suporte@trustcode.com.br',
     url='https://github.com/Trust-Code/PyTrustCnab240',
