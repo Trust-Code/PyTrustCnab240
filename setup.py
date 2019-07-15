8c33a75919281c328b3afef032fa373ec28cc70c
marinaGD <36993009+marinaGD@users.noreply.github.com>
2019-07-15 14:47:21 -0300

    [IMP] Process generic lot header and trailer  (#43)
    * change methods to process generic lot trailer and header

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycnab240',
-    version='0.1.26',
+    version='0.1.27',
     author='Trustcode',
     author_email='suporte@trustcode.com.br',
     url='https://github.com/Trust-Code/PyTrustCnab240',
